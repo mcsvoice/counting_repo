fb9a68cfcf1a364a45b54b7553ab09dd538e6fb9
Michael Cawston-Stewart <michael.cawston.stewart@gmail.com>
2017-07-26 19:51:50 +0000

    major changes later

--- counting.js
+++ counting.js
@@ -1,17 +1,49 @@
-var aString = process.argv.slice(2);
+var aString = process.argv.slice(2).toString();
 var regexAlphabet = /[A-Z]/gi;
-var matchedStuff = aString.match(regexAlphabet);
+var matchedLetters = aString.match(regexAlphabet);
 
-// console.log(aString);
+var theObject = {};
 
-function countLetters(aString){
-  matchedStuff = aString.match(regexAlphabet);
+//console.log(aString);
+//console.log(matchedLetters);
+//console.log(letterCount);
+
+for (var i = 0; i < (matchedLetters.length); i++) {
+
+    if (theObject[matchedLetters[i]]) {
+      theObject[matchedLetters[i]] = theObject[matchedLetters[i]] + 1;
+    } else {
+      theObject[matchedLetters[i]] = 1;
+    }
 }
 
-console.log(countLetters(aString));
+console.log(theObject);
+
+// function countLetters(aString){
+
+// }
+
+// I need to create an object to hold all my data
+// the objects have a "letter" value in them for the letter I just saw in the array
+  // "name" = array[i]
+// the objects have a "count" value in them for each time that letter was seen
+  // "count" = count + 1
+// I then have to return the "letter" and "count" value in a specific pattern when the loop is finished.
+
+// var obj = {};
+
+// obj.first = 1;
+// obj['second-hyphen'] = 2;
+
+// var key = 'third';
+
+// obj.key = 3;
+// obj[key] = 3;
 
 
-
-//my function is CountLetters
-//my function accepts 1 string of letters
-//my function returns 1 string of letters
+// {
+//   first: 1,
+//   second: 2,
+//   key: 3,
+//   third: 3
+// }
